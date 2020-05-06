d470b17da96194d027a1741aeaddc6e5e093db5f
yue1173 <48230057+yue1173@users.noreply.github.com>
2020-05-06 17:54:27 -0400

    pause test

--- app.rb
+++ app.rb
@@ -75,8 +75,11 @@ class CustomHandler < AlexaSkillsRuby::Handler
 
   on_intent("JOKES") do
     # add a response to Alexa
-    response.set_output_speech_text('<speak><amazon:domain name=\'music\'>A bear walks into a bar and says, Give me a whiskey and <break time='3s'/> cola.
-Why the big pause? asks the bartender. The bear shrugged. I am not sure. I was born with them.</amazon:domain></speak>')
+    response.set_output_speech_text("<speak>
+<amazon:domain name=\'music\'> A bear walks into a bar and says, Give me a whiskey and </amazon:domain>
+<break time='3s'/> cola.
+<amazon:domain name=\'music\'>Why the big pause? asks the bartender. The bear shrugged. I am not sure. I was born with them.</amazon:domain>
+</speak>")
     # create a card response in the alexa app
     response.set_simple_card("out of office App", "Status is in the office.")
     # log the output if needed
@@ -213,7 +216,6 @@ end
 # twiml.to_s
 
 # end
-
 
 
 # ----------------------------------------------------------------------
